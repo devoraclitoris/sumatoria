680d83dcfd7fb7fbd1055153be56dc1c94409b2f
devoraclitoris <60493532+devoraclitoris@users.noreply.github.com>
2020-02-05 13:33:29 -0500

    Add files via upload

---
+++ sumar/src/sumar/Sumar.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sumar;
+public class Sumar {
+    public static void main(String[] args) {
+        long res=0;
+        System.out.println("SUMATORIA DE NUMEROS IMPARES DEL 1 HASTA 100000000");
+        res=100000000+1;
+        res=res/2;
+        res=res*res;
+        System.out.println("LA SUMATORIA DE NUMEROS IMPARES DEL 1 HASTA 100000000 ES: "+res);
+    }
+}
